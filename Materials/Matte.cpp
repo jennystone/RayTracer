0ac5f5c6bba729d903d81cb888bd5c16517e4c0b
janani <aspire.janani@gmail.com>
2015-04-13 00:05:37 +0530

    Different materials have been introduced and Phong shading

---
+++ Materials/Matte.cpp
@@ -0,0 +1,108 @@
+#include "Matte.h"
+#include<stdio.h>
+// ---------------------------------------------------------------- default constructor
+
+Matte::Matte (void)
+	:	Material(),
+		ambient_brdf(new Lambertian),
+		diffuse_brdf(new Lambertian)
+{}
+
+
+
+// ---------------------------------------------------------------- copy constructor
+
+Matte::Matte(const Matte& m)
+	: 	Material(m)
+{
+	if(m.ambient_brdf)
+		ambient_brdf = m.ambient_brdf->clone(); 
+	else  ambient_brdf = NULL;
+	
+	if(m.diffuse_brdf)
+		diffuse_brdf = m.diffuse_brdf->clone(); 
+	else  diffuse_brdf = NULL;
+}
+
+
+// ---------------------------------------------------------------- clone
+
+Material*										
+Matte::clone(void) const {
+	return (new Matte(*this));
+}	
+
+
+// ---------------------------------------------------------------- assignment operator
+
+Matte& 
+Matte::operator= (const Matte& rhs) {
+	if (this == &rhs)
+		return (*this);
+		
+	Material::operator=(rhs);
+	
+	if (ambient_brdf) {
+		delete ambient_brdf;
+		ambient_brdf = NULL;
+	}
+
+	if (rhs.ambient_brdf)
+		ambient_brdf = rhs.ambient_brdf->clone();
+		
+	if (diffuse_brdf) {
+		delete diffuse_brdf;
+		diffuse_brdf = NULL;
+	}
+
+	if (rhs.diffuse_brdf)
+		diffuse_brdf = rhs.diffuse_brdf->clone();
+
+	return (*this);
+}
+
+
+// ---------------------------------------------------------------- destructor
+
+Matte::~Matte(void) {
+
+	if (ambient_brdf) {
+		delete ambient_brdf;
+		ambient_brdf = NULL;
+	}
+	
+	if (diffuse_brdf) {
+		delete diffuse_brdf;
+		diffuse_brdf = NULL;
+	}
+}
+
+RGBColor
+Matte::shade(ShadeRec& sr) {
+        Vector3D wo = -sr.ray.d;
+        RGBColor L = ambient_brdf->rho(sr, wo) * sr.w.ambient_ptr->L(sr);
+        int numLights = sr.w.lights.size();
+
+        for(int j = 0; j < numLights; j++) {
+                Vector3D wi = sr.w.lights[j]->get_direction(sr);
+                float ndotwi = sr.normal * wi;
+
+                if (ndotwi > 0.0)       {
+                        bool in_shadow = false;
+                        if (sr.w.lights[j]->casts_shadow()) {
+                                Ray shadow_ray(sr.hit_point, wi);
+                                in_shadow = sr.w.lights[j]->in_shadow(shadow_ray, sr);
+                        }
+
+                        if (!in_shadow)
+                                L+=diffuse_brdf->f(sr, wo, wi) * sr.w.lights[j]->L(sr) * ndotwi;
+                }
+        }
+
+        return (L);
+}
+
+
+// ---------------------------------------------------------------- shade
+
+
---
+++ Materials/Phong.h
@@ -0,0 +1,110 @@
+#ifndef __Phong__
+#define __Phong__
+
+#include "Material.h"
+#include "Lambertian.h"
+#include "GlossySpecular.h"
+
+//----------------------------------------------------------------------------- class Phong
+
+class Phong: public Material {
+	public:
+			
+		Phong(void);
+
+		Phong(const Phong& m);
+		
+		virtual Material*										
+		clone(void) const;									
+
+		Phong&
+		operator= (const Phong& rhs);
+
+		~Phong(void);
+		
+		void 													
+		set_ka(const float k);
+		
+		void 													
+		set_kd(const float k);
+		
+		void
+		set_exp(const float k);
+
+		void													
+		set_cd(const RGBColor c);
+		
+		void													
+		set_ks(const float k);
+
+		void
+		set_cd(const float r, const float g, const float b);
+		
+		void																						
+		set_cd(const float c);
+				
+		virtual RGBColor										
+		shade(ShadeRec& sr);
+		
+	private:
+		
+		Lambertian*		ambient_brdf;
+		Lambertian*		diffuse_brdf;
+		GlossySpecular* specular_brdf;
+};
+
+
+// ---------------------------------------------------------------- set_ka
+// this sets Lambertian::kd
+// there is no Lambertian::ka data member because ambient reflection 
+// is diffuse reflection
+
+inline void								
+Phong::set_ka(const float ka) {
+	ambient_brdf->set_kd(ka);
+}
+
+
+// ---------------------------------------------------------------- set_kd
+// this also sets Lambertian::kd, but for a different Lambertian object
+
+inline void								
+Phong::set_kd (const float kd) {
+	diffuse_brdf->set_kd(kd);
+}
+
+inline void
+Phong::set_exp (const float exp) {
+	specular_brdf->set_exp(exp);
+}
+
+inline void
+Phong::set_ks (const float ks) {
+	specular_brdf->set_ks(ks);
+}
+// ---------------------------------------------------------------- set_cd
+
+inline void												
+Phong::set_cd(const RGBColor c) {
+	ambient_brdf->set_cd(c);
+	diffuse_brdf->set_cd(c);
+}
+
+
+// ---------------------------------------------------------------- set_cd
+
+inline void													
+Phong::set_cd(const float r, const float g, const float b) {
+	ambient_brdf->set_cd(r, g, b);
+	diffuse_brdf->set_cd(r, g, b);
+}
+
+// ---------------------------------------------------------------- set_cd
+
+inline void													
+Phong::set_cd(const float c) {
+	ambient_brdf->set_cd(c);
+	diffuse_brdf->set_cd(c);
+}
+
+#endif
